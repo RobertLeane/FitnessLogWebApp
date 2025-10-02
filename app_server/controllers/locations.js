0e40197c09bc49a0b9c88f21cdd9de249b7c2c0c
Robert Leane <leane.rob@gmail.com>
2025-10-02 10:54:22 +0100

    Location.js update

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,32 @@
 /* GET 'home' page */
 const homelist = function(req, res){
-res.render('index', { title: 'Fitness Log App' });
+res.render('index', {
+  title: 'Fitness log App',
+  pageHeader: { 
+    title: 'Hi Robert...', 
+    strapline: 'Track your workouts and progress' 
+  },
+  workouts: [{ 
+    title: 'Running',
+    duration: '60 Minutes',
+    description: 'Morning run in the park',
+    intensity: 'High Intensity',
+    date: 'Sep 16 2025'
+  },{
+    title: 'Weight Training',
+    duration: '45 Minutes',
+    description: 'Chest and triceps focus',
+    intensity: 'Very High Intensity',
+    date: 'Sep 17 2025'
+  },{
+    title: 'Yoga',
+    duration: '30 Minutes',
+    description: 'Evening relaxation session',
+    intensity: 'Low Intensity',
+    date: 'Sep 17 2025'
+  }]
+  });
+
 };
 
 module.exports = {
