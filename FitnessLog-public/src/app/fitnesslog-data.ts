7c41b06c009f1f7832f96eea0ccc22712b1fa012
Robert Leane <leane.rob@gmail.com>
2025-11-20 11:07:05 +0000

    Additional Work

--- FitnessLog-public/src/app/fitnesslog-data.ts
+++ FitnessLog-public/src/app/fitnesslog-data.ts
@@ -8,6 +8,8 @@ import { Workout } from './workouts-list/workouts-list';
 export class FitnessLogData {
   constructor(private http: HttpClient) { }
 
+  private apiBaseUrl = 'http://localhost:3000/api/';
+
   public getWorkouts() : Promise<Workout[]> {
     // Our code will go here
     return Promise.resolve([]);
